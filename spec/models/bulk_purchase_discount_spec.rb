f0cb8cf4d39c71f5c8a7d5d3ccab2911a1f5fa6f
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:34:01 +0200

    Add test to verify BulkPurchaseDiscount class definition
    This commit will add a test to ensure that the `BulkPurchaseDiscount`
    class is defined

--- spec/models/bulk_purchase_discount_spec.rb
+++ spec/models/bulk_purchase_discount_spec.rb
@@ -2,5 +2,7 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/bulk_purchase_discount'
 
 describe 'BulkPurchaseDiscount' do
-  
+  it 'should be a class' do
+    expect(Object.const_defined?('BulkPurchaseDiscount')).to be(true)
+  end
 end
