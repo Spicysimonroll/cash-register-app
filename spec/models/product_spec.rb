9ef05b84e27b4963d73e2c688a5c4086f3b1d12f
simon hu <simon.hu.1999@gmail.com>
2024-04-30 18:53:38 +0200

    Add setup for Product test data generation
    - Initialize `empty_product` instance without parameter
    - Initialize `product` instance with specific parameters
    
    This commit will add setup code to create instances of `Product` class
    for use in testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../../lib/models/product'
 
 describe 'Product' do
+  let(:empty_product) { Product.new }
+  let(:product) { Product.new({ code: 'CF1', name: 'Coffee', price: 11.23 }) }
+
   it 'should be a class' do
     expect(Object.const_defined?('Product')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "empty_product: #{empty_product.inspect}"
+    puts "product: #{product.inspect}"
   end
 end
