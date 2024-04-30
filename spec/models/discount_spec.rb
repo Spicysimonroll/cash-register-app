cf46bd0833d28fc5bb3de15d8a5e1846c0647b0a
simon hu <simon.hu.1999@gmail.com>
2024-04-30 19:31:39 +0200

    Add setup for Discount test data generation
    - Initialize `discount` instance with specific parameters
    
    This commit will add setup code to create instances of `Discount` class
    for use in testing

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../../lib/models/discount'
 
 describe 'Discount' do
+  let(:discount) { Discount.new('€5 discount', 5) }
+
   it 'should be a class' do
     expect(Object.const_defined?('Discount')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "discount: #{discount.inspect}"
   end
 end
