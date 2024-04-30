4d58104a335ababc98167449c42bcf4a73872681
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:21:13 +0200

    Add setup for PercentageDiscount test data generation
    - Initialize `percentage_discount` instance with specific
      parameters
    
    This commit will add setup code to create instances of
    `PercentageDiscount` class for use in testing

--- spec/models/percentage_discount_spec.rb
+++ spec/models/percentage_discount_spec.rb
@@ -2,11 +2,17 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/percentage_discount'
 
 describe 'PercentageDiscount' do
+  let(:percentage_discount) { PercentageDiscount.new('Buy 3 and pay 2/3 of the original price', 0, (1 / 3.0), 3) }
+
   it 'should be a class' do
     expect(Object.const_defined?('PercentageDiscount')).to be(true)
   end
 
   it 'should be inheriting Discount class' do
     expect(PercentageDiscount.superclass).to be(Discount)
+  end
+
+  it 'should generate test data' do
+    puts "percentage_discount: #{percentage_discount.inspect}"
   end
 end
