79f166824482af21599ffc7f4c0a0a674b4b1efe
simon hu <simon.hu.1999@gmail.com>
2024-04-30 23:29:04 +0200

    Define :initialize method for BulkPurchaseDiscount class
    This commit will add the `initialize` method to the `BulkPurchaseDiscount`
    class. The method initializes instance variables `@name`, `@amount`,
    `@discounted_price` and `@threshold` based on `name`, `amount`, `new_price`
    and `threshold` parameters provided during object instantiation

--- lib/models/bulk_purchase_discount.rb
+++ lib/models/bulk_purchase_discount.rb
@@ -1,2 +1,7 @@
 class BulkPurchaseDiscount < Discount
+  def initialize(name, amount, new_price, threshold)
+    super(name, amount)
+    @discounted_price = new_price
+    @threshold = threshold
+  end
 end
