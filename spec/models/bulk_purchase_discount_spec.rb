837d515e185e0dce5de11c18c212ec4985e1a5ef
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:40:31 +0200

    Add setup for BulkPurchaseDiscount test data generation
    - Initialize `bulk_purchase_discount` instance with specific
      parameters
    
    This commit will add setup code to create instances of
    `BulkPurchaseDiscount` class for use in testing

--- spec/models/bulk_purchase_discount_spec.rb
+++ spec/models/bulk_purchase_discount_spec.rb
@@ -2,11 +2,17 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/bulk_purchase_discount'
 
 describe 'BulkPurchaseDiscount' do
+  let(:bulk_purchase_discount) { BulkPurchaseDiscount.new('Buy 3 and pay €4.5 each', 0, 4.5, 3) }
+
   it 'should be a class' do
     expect(Object.const_defined?('BulkPurchaseDiscount')).to be(true)
   end
 
   it 'should be inheriting Discount class' do
     expect(BulkPurchaseDiscount.superclass).to be(Discount)
+  end
+
+  it 'should generate test data' do
+    puts "bulk_purchase_discount: #{bulk_purchase_discount.inspect}"
   end
 end
