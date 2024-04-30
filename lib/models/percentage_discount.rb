bf7058bcdd9771bb74b3e6549c964a33cc74a667
simon hu <simon.hu.1999@gmail.com>
2024-04-30 23:20:27 +0200

    Define :initialize method for PercentageDiscount class
    This commit will add the `initialize` method to the `PercentageDiscount`
    class. The method initializes instance variables `@name`, `@amount`,
    `@percentage` and `@threshold` based on `name`, `amount`, `percentage`
    and `threshold` parameters provided during object instantiation

--- lib/models/percentage_discount.rb
+++ lib/models/percentage_discount.rb
@@ -1,2 +1,7 @@
 class PercentageDiscount < Discount
+  def initialize(name, amount, percentage, threshold)
+    super(name, amount)
+    @percentage = percentage
+    @threshold = threshold
+  end
 end
