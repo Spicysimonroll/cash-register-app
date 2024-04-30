2c37da7a7f1691334be5e2c0565ad640ef5c5dd4
simon hu <simon.hu.1999@gmail.com>
2024-04-30 23:14:18 +0200

    Define :apply method for BuyOneGetOneFreeDiscount class
      This commit will add the `apply` method to the `BuyOneGetOneFreeDiscount`
      class. The method takes `original_price` and `quantity` as parameters,
      and it checks if the `quantity` is even or odd and applies the
      appropriate discount accordingly

--- lib/models/buy_one_get_one_free_discount.rb
+++ lib/models/buy_one_get_one_free_discount.rb
@@ -1,2 +1,5 @@
 class BuyOneGetOneFreeDiscount < Discount
+  def apply(original_price, quantity)
+    (quantity % 2).zero? ? (quantity / 2 * original_price) : ((quantity / 2) + 1) * original_price
+  end
 end
