307dad19c88a712fa239106c5bea74f2480a346d
simon hu <simon.hu.1999@gmail.com>
2024-04-30 23:31:15 +0200

    Define :apply method for BulkPurchaseDiscount class
    This commit will add the `apply` method to the `BulkPurchaseDiscount`
    class. The method takes `original_price` and `product_quantity` as
    parameters. It calculates the final price based on `product_quantity`,
    applying a `@discounted_price` per unit if the quantity meets or exceeds
    `@threshold`

--- lib/models/bulk_purchase_discount.rb
+++ lib/models/bulk_purchase_discount.rb
@@ -4,4 +4,8 @@ class BulkPurchaseDiscount < Discount
     @discounted_price = new_price
     @threshold = threshold
   end
+
+  def apply(original_price, product_quantity)
+    product_quantity < @threshold ? product_quantity * original_price : product_quantity * @discounted_price
+  end
 end
