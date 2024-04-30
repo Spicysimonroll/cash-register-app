098d29fd24118c5b766a7e7ee1eac364d08f1be1
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:36:04 +0200

    Add test to verify BulkPurchaseDiscount class inheritance
    This commit will add a test to ensure that the `BulkPurchaseDiscount`
    class is inheriting from the `Discount`

--- spec/models/bulk_purchase_discount_spec.rb
+++ spec/models/bulk_purchase_discount_spec.rb
@@ -5,4 +5,8 @@ describe 'BulkPurchaseDiscount' do
   it 'should be a class' do
     expect(Object.const_defined?('BulkPurchaseDiscount')).to be(true)
   end
+
+  it 'should be inheriting Discount class' do
+    expect(BulkPurchaseDiscount.superclass).to be(Discount)
+  end
 end
