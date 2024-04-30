3cc5dbb190702eaeb3ddf8835fb2b313e041f95e
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:53:09 +0200

    Define :initialize method for Product class
    This commit will add the `initialize` method to the `Product` class. The
    method initializes instance variables `@code`, `@name` and `@price` based
    on attributes provided in an optional `attr` hash during object
    instantiation

--- lib/models/product.rb
+++ lib/models/product.rb
@@ -1,2 +1,7 @@
 class Product
+  def initialize(attr = {})
+    @name = attr.empty? ? nil : attr[:name].to_s
+    @code = attr.empty? ? nil : attr[:code].to_s
+    @price = attr.empty? ? nil : attr[:price].to_f
+  end
 end
