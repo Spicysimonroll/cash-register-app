5c85d5ca2167a49a7ba47bdf447b414190490db5
simon hu <simon.hu.1999@gmail.com>
2024-04-30 21:58:32 +0200

    Add test to verify BuyOneGetOneFreeDiscount class definition
    This commit will add a test to ensure that the `BuyOneGetOneFreeDiscount`
    class is defined

--- spec/models/buy_one_get_one_free_discount_spec.rb
+++ spec/models/buy_one_get_one_free_discount_spec.rb
@@ -2,5 +2,7 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/buy_one_get_one_free_discount'
 
 describe 'BuyOneGetOneFreeDiscount' do
-
+  it 'should be a class' do
+    expect(Object.const_defined?('BuyOneGetOneFreeDiscount')).to be(true)
+  end
 end
