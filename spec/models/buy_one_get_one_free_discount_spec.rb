1bdee98404a7aadc70582fa665dffe11057d19bf
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:01:28 +0200

    Add setup for BuyOneGetOneFreeDiscount test data generation
    - Initialize `buy_one_get_one_free_discount` instance with specific
      parameters
    
    This commit will add setup code to create instances of
    `BuyOneGetOneFreeDiscount` class for use in testing

--- spec/models/buy_one_get_one_free_discount_spec.rb
+++ spec/models/buy_one_get_one_free_discount_spec.rb
@@ -2,11 +2,17 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/buy_one_get_one_free_discount'
 
 describe 'BuyOneGetOneFreeDiscount' do
+  let(:buy_one_get_one_free_discount) { BuyOneGetOneFreeDiscount.new('Buy one green tea and get one for free', 0) }
+
   it 'should be a class' do
     expect(Object.const_defined?('BuyOneGetOneFreeDiscount')).to be(true)
   end
 
   it 'should be inheriting Discount class' do
     expect(BuyOneGetOneFreeDiscount.superclass).to be(Discount)
+  end
+
+  it 'should generate test data' do
+    puts "buy_one_get_one_free_discount: #{buy_one_get_one_free_discount.inspect}"
   end
 end
