329050101cc892877bbe7020a04fcf166899c984
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:09:58 +0200

    Add tests for BuyOneGetOneFreeDiscount#initialize
    - Verify BuyOneGetOneFreeDiscount#initialize method's arity
    - Ensure that the instance variables are initialized correctly in
      `BuyOneGetOneFreeDiscount` instances
    
    This commit will add tests within the '#initialize' describe block for
    the `BuyOneGetOneFreeDiscount` class

--- spec/models/buy_one_get_one_free_discount_spec.rb
+++ spec/models/buy_one_get_one_free_discount_spec.rb
@@ -15,4 +15,21 @@ describe 'BuyOneGetOneFreeDiscount' do
   it 'should generate test data' do
     puts "buy_one_get_one_free_discount: #{buy_one_get_one_free_discount.inspect}"
   end
+
+  describe '#initialize' do
+    it 'should take two arguments' do
+      expect(BuyOneGetOneFreeDiscount.instance_method(:initialize).arity).to eq(2)
+      expect(buy_one_get_one_free_discount).to be_a(BuyOneGetOneFreeDiscount)
+    end
+
+    shared_examples 'attribute initialization' do |attribute, type|
+      it "should initialize the instance with a #{attribute} variable" do
+        expect(buy_one_get_one_free_discount.instance_variable_defined?(attribute)).to be(true)
+        expect(buy_one_get_one_free_discount.instance_variable_get(attribute)).to be_a(type)
+      end
+    end
+
+    include_examples 'attribute initialization', :@name, String
+    include_examples 'attribute initialization', :@amount, Integer
+  end
 end
