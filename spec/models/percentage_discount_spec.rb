f80604c48b2c1543d961abb11cab99e5b675dacf
simon hu <simon.hu.1999@gmail.com>
2024-04-30 22:18:34 +0200

    Add test to verify PercentageDiscount class definition
      This commit will add a test to ensure that the `PercentageDiscount`
      class is defined

--- spec/models/percentage_discount_spec.rb
+++ spec/models/percentage_discount_spec.rb
@@ -2,5 +2,7 @@ require_relative '../../lib/models/discount'
 require_relative '../../lib/models/percentage_discount'
 
 describe 'PercentageDiscount' do
-  
+  it 'should be a class' do
+    expect(Object.const_defined?('PercentageDiscount')).to be(true)
+  end
 end
