3a04b05689e2335820d829383a1809d5aef9e35d
simon hu <simon.hu.1999@gmail.com>
2024-05-01 14:11:41 +0200

    Add setup for View test data generation
    - Initialize `view` instance with no arguments
    
    This commit will add setup code to create an instance of `View` class
    for use in testing

--- spec/views/view_spec.rb
+++ spec/views/view_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../../lib/views/view'
 
 describe 'View' do
+  let(:view) { View.new }
+
   it 'should be a class' do
     expect(Object.const_defined?('View')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "view: #{view.inspect}"
   end
 end
