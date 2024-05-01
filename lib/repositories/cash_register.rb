dcbb06098bf8378050b43d15b71e10472d4c1fe1
simon hu <simon.hu.1999@gmail.com>
2024-05-01 11:38:03 +0200

    Define :initialize method for CashRegister class
    - Initialize `@inventory_csv` instance variable with `inventory_csv`
    - Initialize `@cart_csv` instance variable with `cart_csv`
    - Initialize `@inventory` instance variable as an empty array
    - Initialize `@cart` instance variable as an empty array
    - Call :load_inventory private method to load data from `@inventory_csv`
      into `@inventory` instance variable
    - Call :load_cart private method to load data from `@cart_csv` into
      `@cart` instance variable
    
    This commit will add the `initialize` method to the `CashRegister`class.
    The method is defined with `inventory_csv` and `cart_csv` parameters
    that will be provided during object instantiation and stored into
    instance variables.
    This commit will also add :load_inventory and :load_cart private methods
    in order to provide support to the :initialize method

--- lib/repositories/cash_register.rb
+++ lib/repositories/cash_register.rb
@@ -1,2 +1,29 @@
+require 'csv'
+require_relative '../models/product'
+
 class CashRegister
+  attr_reader :inventory, :cart
+
+  def initialize(inventory_csv, cart_csv)
+    @inventory_csv = inventory_csv
+    @cart_csv = cart_csv
+    @inventory = []
+    @cart = []
+    load_inventory
+    load_cart
+  end
+
+  private
+
+  def load_inventory
+    CSV.foreach(@inventory_csv, headers: true) { |row| @inventory << Product.new({ code: row[0], name: row[1], price: row[2] }) }
+  end
+
+  def load_cart
+    CSV.foreach(@cart_csv, headers: true) do |row|
+      quantity = row[0].to_i
+      product_code = row[1]
+      quantity.times { @cart << product_code }
+    end
+  end
 end
