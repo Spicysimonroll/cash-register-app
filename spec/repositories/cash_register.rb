6d100f61a6b5ee593178013af5e22eefadfe5f5b
simon hu <simon.hu.1999@gmail.com>
2024-05-01 10:14:56 +0200

    Add test to verify CashRegister class definition
    This commit will add a test to ensure that the `CashRegister` class is
    defined

--- spec/repositories/cash_register.rb
+++ spec/repositories/cash_register.rb
@@ -1,5 +1,7 @@
 require_relative '../../lib/repositories/cash_register'
 
 describe 'CashRegister' do
-  
+  it 'should be a class' do
+    expect(Object.const_defined?('CashRegister')).to be(true)
+  end
 end
