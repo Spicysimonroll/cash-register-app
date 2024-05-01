4e663fe06af3588669be64007c6a6add8c954bc6
simon hu <simon.hu.1999@gmail.com>
2024-05-01 15:46:47 +0200

    Define :display_cart method for View class
    This commit will add the `display_cart` method to the `View` class.
    The method takes `cart` and `tot` as a parameter and displays the cart

--- lib/views/view.rb
+++ lib/views/view.rb
@@ -1,2 +1,22 @@
 class View
+  def display_cart(cart, tot = 0)
+    if cart.empty?
+      puts '+--------------------+'
+      puts '| Your cart is empty |'
+      puts '+----------+---------+'
+    else
+      puts '+--------------------+'
+      puts '|      Your cart     |'
+      puts '+----------+---------+'
+      puts '| quantity | product |'
+      puts '+--------------------+'
+      cart.uniq.each do |product_code|
+        count = cart.count(product_code)
+        puts "|#{' ' * 4}#{count}#{' ' * (6 - count.to_s.size)}|#{' ' * 3}#{product_code}#{' ' * 3}|"
+        puts '+--------------------+'
+      end
+      puts "| total#{' ' * (11 - tot.to_s.size)}€#{tot}  |"
+      puts '+--------------------+'
+    end
+  end
 end
