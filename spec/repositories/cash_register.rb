07141c220df76ca54f3bf217303ee447c5e4555c
simon hu <simon.hu.1999@gmail.com>
2024-05-01 10:26:19 +0200

    Add setup for CashRegister test data generation
    - Initialize `inventory_csv` variable containing the path for the
      inventory.csv file
    - Initialize `cart1_csv` variable containing the path for cart1.csv file
    - Initialize `cart2_csv` variable containing the path for cart2.csv file
    - Initialize `cart3_csv` variable containing the path for cart3.csv file
    - Initialize `inventory_with_headers` variable containing data to be
      written in `spec/support/inventory.csv` file
    - Initialize `cart1_with_headers` variable containing data to be
      written in `spec/support/cart1.csv` file
    - Initialize `cart2_with_headers` variable containing data to be
      written in `spec/support/cart2.csv` file
    - Initialize `cart3_with_headers` variable containing data to be
      written in `spec/support/cart3.csv` file
    
    This commit will add setup code to create variables for use in testing

--- spec/repositories/cash_register.rb
+++ spec/repositories/cash_register.rb
@@ -1,7 +1,52 @@
 require_relative '../../lib/repositories/cash_register'
 
 describe 'CashRegister' do
+  let(:inventory_csv) { 'spec/support/inventory.csv' }
+  let(:cart1_csv) { 'spec/support/cart1.csv' }
+  let(:cart2_csv) { 'spec/support/cart2.csv' }
+  let(:cart3_csv) { 'spec/support/cart3.csv' }
+  let(:inventory_with_headers) do
+    [
+      ['code', 'product name', 'price(€)'],
+      ['GR1', 'Green Tea', 3.11],
+      ['SR1', 'Strawberries', 5.00],
+      ['CF1', 'Coffee', 11.23]
+    ]
+  end
+  let(:cart1_with_headers) do
+    [
+      ['quantity', 'product'],
+      [2, 'GR1']
+    ]
+  end
+  let(:cart2_with_headers) do
+    [
+      ['quantity', 'product'],
+      [3, 'SR1'],
+      [1, 'GR1']
+    ]
+  end
+  let(:cart3_with_headers) do
+    [
+      ['quantity', 'product'],
+      [1, 'GR1'],
+      [3, 'CF1'],
+      [1, 'SR1']
+    ]
+  end
+
   it 'should be a class' do
     expect(Object.const_defined?('CashRegister')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "inventory_csv: #{inventory_csv.inspect}"
+    puts "cart1_csv: #{cart1_csv.inspect}"
+    puts "cart2_csv: #{cart2_csv.inspect}"
+    puts "cart3_csv: #{cart3_csv.inspect}"
+    puts "inventory_with_headers: #{inventory_with_headers.inspect}"
+    puts "cart1_with_headers: #{cart1_with_headers.inspect}"
+    puts "cart2_with_headers: #{cart2_with_headers.inspect}"
+    puts "cart3_with_headers: #{cart3_with_headers.inspect}"
   end
 end
