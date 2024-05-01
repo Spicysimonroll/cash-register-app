07c90585f77c479d37a4b1e568d70c949d440911
simon hu <simon.hu.1999@gmail.com>
2024-05-01 14:44:15 +0200

    Add setup for Controller test data generation
    - Initialize `inventory_csv` variable with `spec/support/inventory.csv`
      file path
    - Initialize `cart3_csv` variable with `spec/support/cart3.csv` file
      path
    - Initialize `cash_register` instance with `inventory_csv` and `cart3_csv`
      as arguments
    - Initialize `controller` instance with `cash_register` instance as
      argument
    - Initialize `cart3_with_headers` variable containing data to be
      written in `spec/support/cart3.csv` file
    
    This commit will add setup code to create variables for use in testing

--- spec/controllers/controller_spec.rb
+++ spec/controllers/controller_spec.rb
@@ -1,7 +1,28 @@
 require_relative '../../lib/controllers/controller'
+require_relative '../../lib/repositories/cash_register'
 
 describe 'Controller' do
+  let(:inventory_csv) { 'spec/support/inventory.csv' }
+  let(:cart3_csv) { 'spec/support/cart3.csv' }
+  let(:cash_register) { CashRegister.new(inventory_csv, cart3_csv) }
+  let(:controller) { Controller.new(cash_register) }
+  let(:cart3_with_headers) do
+    [
+      ['quantity', 'product'],
+      [1, 'GR1'],
+      [3, 'CF1'],
+      [1, 'SR1']
+    ]
+  end
+
   it 'should be a class' do
     expect(Object.const_defined?('Controller')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "inventory_csv: #{inventory_csv.inspect}"
+    puts "cart3_csv: #{cart3_csv.inspect}"
+    puts "cash_register: #{cash_register.inspect}"
+    puts "controller: #{controller.inspect}"
   end
 end
