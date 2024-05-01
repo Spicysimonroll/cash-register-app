149d659fc323e145d0f734b66a52d7b08897402f
simon hu <simon.hu.1999@gmail.com>
2024-05-01 14:39:43 +0200

    Add test to verify Controller class definition
    This commit will add a test to ensure that the `Controller` class is
    defined

--- spec/controllers/controller_spec.rb
+++ spec/controllers/controller_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../../lib/controllers/controller'
 
 describe 'Controller' do
-  
+  it 'should be a class' do
+    expect(Object.const_defined?('Controller')).to be(true)
+  end
 end
