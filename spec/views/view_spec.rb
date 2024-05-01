cab8d4e48a799610650773e4cbd7f6657312c2e9
simon hu <simon.hu.1999@gmail.com>
2024-05-01 14:18:34 +0200

    Add tests for View#display_cart
    - Verify View#display_cart method's arity
    - Ensure that View#display_cart is correctly displaying the cart passed
      as an argument when the method is called
    
    This commit will add tests within the '#display_cart' describe block for
    the `View` class

--- spec/views/view_spec.rb
+++ spec/views/view_spec.rb
@@ -10,4 +10,27 @@ describe 'View' do
   it 'should generate test data' do
     puts "view: #{view.inspect}"
   end
+
+  describe '#display_cart' do
+    it 'should take one or two arguments (the cart and the total price)' do
+      expect(view).to respond_to(:display_cart)
+      expect(View.instance_method(:display_cart).arity).to eq(-2)
+    end
+
+    it 'should implement a method to display the products in the cart' do
+      cart = %w[GR1 GR1 SR1 CF1 SR1]
+      allow($stdout).to receive(:puts)
+      view.display_cart(cart, 24.34)
+
+      expect($stdout).to have_received(:puts).with(/.*2.*GR1.*/)
+      expect($stdout).to have_received(:puts).with(/.*2.*SR1.*/)
+      expect($stdout).to have_received(:puts).with(/.*1.*CF1.*/)
+      expect($stdout).to have_received(:puts).with(/.*total.*€24.34.*/)
+
+      empty_cart = []
+      view.display_cart(empty_cart)
+
+      expect($stdout).to have_received(:puts).with(/.*cart.*empty.*/i)
+    end
+  end
 end
