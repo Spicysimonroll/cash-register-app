ced1f55c015db82b25460bc67f28e14bcd50083d
simon hu <simon.hu.1999@gmail.com>
2024-05-01 16:02:51 +0200

    Define :add_product_to_cart method for Controller class
    This commit will add the `add_product_to_cart` method to the `Controller`
    class. The method takes no arguments. it displays the inventory, let the
    user choose the product to add, return the chosen product's index and
    add it to the cart, in case the index provided exists. An error message
    is displayed if the index provided by the user does exist

--- lib/controllers/controller.rb
+++ lib/controllers/controller.rb
@@ -9,4 +9,16 @@ class Controller
   def display_cart_products
     @view.display_cart(@cash_register.cart, @cash_register.total_price)
   end
+
+  def add_product_to_cart
+    inventory = @cash_register.inventory
+    @view.display_inventory(inventory)
+    index = @view.ask_for(:add)
+    if index > -1 && index < inventory.size
+      product = inventory[index]
+      @cash_register.scan(product)
+    else
+      @view.display_message('ERROR! The index provided does not exist')
+    end
+  end
 end
