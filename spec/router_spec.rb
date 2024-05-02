ece724c4746e8230d1b0bb8802241e3780ec74b7
simon hu <simon.hu.1999@gmail.com>
2024-05-02 09:10:45 +0200

    Add setup for Router test data generation
    - Initialize `controller` variable with an instance double of the class
      `Controller`
    - Initialize `router` variable with an instance of `Router` with the
      variable `controller` as an argument
    
    This commit will add setup code to create variables for use in testing

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,7 +1,23 @@
 require_relative '../lib/router'
+require_relative '../lib/controllers/controller'
+require_relative '../lib/repositories/cash_register'
 
 describe 'Router' do
+  let(:inventory_csv) { 'spec/support/inventory.csv'}
+  let(:cart3_csv) { 'spec/support/cart3.csv'}
+  let(:cash_register) { CashRegister.new(inventory_csv, cart3_csv) }
+  let(:controller) { Controller.new(cash_register) }
+  let(:router) { Router.new(controller) }
+
   it 'should be a class' do
     expect(Object.const_defined?('Router')).to be(true)
+  end
+
+  it 'should generate test data' do
+    puts "inventory_csv: #{inventory_csv.inspect}"
+    puts "cart3_csv: #{cart3_csv.inspect}"
+    puts "cash_register: #{cash_register.inspect}"
+    puts "controller: #{controller.inspect}"
+    puts "router: #{router.inspect}"
   end
 end
