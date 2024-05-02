47e5afd359520e9134a3e4cf3d60dfdfd9b3d4b5
simon hu <simon.hu.1999@gmail.com>
2024-05-02 13:51:54 +0200

    Create app.rb
    This commit will create `app.rb` and defined the required code to make
    the app functioning. Specifically, it will:
    
    - Initialize `inventory_csv` variable with the path to `inventory.csv`
    - Initialize `cart_csv` variable with the path to `cart.csv`
    - Initialize `controller` variable with a new instance of `Controller`,
      passing `inventory_csv` and `cart_csv` variables as arguments for the
      istantiation
    - Initialize `router` variable with a new instance of `Router`, passing
      `controller` variable as an argument for the instantiation
    - Call the method :run on `router` to run the application

---
+++ app.rb
@@ -0,0 +1,12 @@
+require_relative 'lib/repositories/cash_register'
+require_relative 'lib/controllers/controller'
+require_relative 'lib/router'
+
+inventory_csv = File.join(__dir__, 'data/inventory.csv')
+cart_csv = File.join(__dir__, 'data/cart.csv')
+cash_register = CashRegister.new(inventory_csv, cart_csv)
+controller = Controller.new(cash_register)
+
+router = Router.new(controller)
+
+router.run
