edb183f8ce2a3de6115b4cf5f0ee497a74f964e2
simon hu <simon.hu.1999@gmail.com>
2024-05-02 10:29:09 +0200

    Add tests for Router#run
    - Verify Router#run method's arity
    - Ensure that the method is calling the correct controller's action based
      on the input provided by the user
    
    This commit will add tests within the '#run' describe block for the
    `Router` class

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -37,4 +37,78 @@ describe 'Router' do
     include_examples 'instance variable initialization', :@controller, Controller
     include_examples 'instance variable initialization', :@running, TrueClass
   end
+
+  describe '#run' do
+    it 'should take no arguments' do
+      expect(router).to respond_to(:run)
+      expect(Router.instance_method(:run).arity).to eq(0)
+    end
+
+    context 'when the input is 1' do
+      it 'calls :add_product_to_cart on controller' do
+        expect(controller).to receive(:add_product_to_cart).once
+        expect(controller).not_to receive(:remove_product_from_cart)
+        expect(controller).not_to receive(:checkout)
+        allow(router).to receive(:gets).and_return("1\n", "4\n")
+        allow($stdout).to receive(:puts)
+        router.run
+
+        expect($stdout).to have_received(:puts).with(/what do you want to do next?/i).twice
+        expect(router.instance_variable_get(:@running)).to be(false)
+      end
+    end
+
+    context 'when the input is 2' do
+      it 'calls :remove_product_from_cart on controller' do
+        expect(controller).to receive(:remove_product_from_cart).once
+        expect(controller).not_to receive(:add_product_to_cart)
+        expect(controller).not_to receive(:checkout)
+        allow($stdout).to receive(:puts)
+        allow(router).to receive(:gets).and_return("2\n", "4\n")
+        router.run
+
+        expect($stdout).to have_received(:puts).with(/what do you want to do next?/i).twice
+        expect(router.instance_variable_get(:@running)).to be(false)
+      end
+    end
+
+    context 'when the input is 3' do
+      it 'calls :checkout on controller' do
+        expect(controller).to receive(:checkout).once
+        expect(controller).not_to receive(:add_product_to_cart)
+        expect(controller).not_to receive(:remove_product_from_cart)
+        allow(router).to receive(:gets).and_return("3\n", "4\n")
+        allow($stdout).to receive(:puts)
+        router.run
+
+        expect($stdout).to have_received(:puts).with(/what do you want to do next?/i).twice
+        expect(router.instance_variable_get(:@running)).to be(false)
+      end
+    end
+
+    context 'when input is 4' do
+      it 'exits the app' do
+        expect(controller).not_to receive(:add_product_to_cart)
+        expect(controller).not_to receive(:remove_product_from_cart)
+        expect(controller).not_to receive(:checkout)
+        allow(router).to receive(:gets).and_return("4\n")
+        allow($stdout).to receive(:puts)
+        router.run
+
+        expect($stdout).to have_received(:puts).with(/what do you want to do next?/i).once
+        expect(router.instance_variable_get(:@running)).to be(false)
+      end
+    end
+
+    context 'when input is not a number between 1-4' do
+      it 'displays an error message' do
+        allow(router).to receive(:gets).and_return("-1\n", "4\n")
+        allow($stdout).to receive(:puts)
+        router.run
+
+        expect($stdout).to have_received(:puts).with(/.*1-4.*/i).once
+        expect(router.instance_variable_get(:@running)).to be(false)
+      end
+    end
+  end
 end
